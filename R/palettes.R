339cd968185b920046cf56d8dfb30887d2abd7d3
robchoudhury <robchoudhury@ucdavis.edu>
2019-08-30 15:26:02 -0500

    updated palettes with more colorblind friendly

--- R/palettes.R
+++ R/palettes.R
@@ -9,3 +9,16 @@ palette_OkabeIto <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D
 #' @rdname palette_OkabeIto
 #' @export
 palette_OkabeIto_black <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#000000")
+
+#From Paul Tol: https://personal.sron.nl/~pault/
+#' @rdname palette_Tol_bright
+#' @export
+palette_Tol_bright <- c('#EE6677', '#228833', '#4477AA', '#CCBB44', '#66CCEE', '#AA3377', '#BBBBBB')
+
+#' @rdname palette_Tol_muted
+#' @export
+palette_Tol_muted <- c('#88CCEE', '#44AA99', '#117733', '#332288', '#DDCC77', '#999933','#CC6677', '#882255', '#AA4499', '#DDDDDD')
+
+#' @rdname palette_Tol_light
+#' @export
+palette_Tol_light <- c('#BBCC33', '#AAAA00', '#77AADD', '#EE8866', '#EEDD88', '#FFAABB', '#99DDFF', '#44BB99', '#DDDDDD')
